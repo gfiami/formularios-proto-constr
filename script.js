c24c0c0a068ef5418ff9f0dc1d947c075abaf2bd
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2022-11-07 21:40:10 -0300

    adding new functions to get user input

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-function noRefreshPage() {}
+function functionRedirect() {
+  getValues();
+  clearInputs();
+}
+
+function getValues() {
+  let fullName = document.querySelector('[name="fname"]').value;
+  console.log(fullName);
+  let birthDate = document.querySelector('[name="bdate"]').value;
+  console.log(birthDate);
+  let sexType = document.querySelector('[name="sex"]').value;
+  console.log(sexType);
+}
+
+function clearInputs() {
+  document.querySelector('[name="fname"]').value = "";
+  document.querySelector('[name="bdate"]').value = "";
+  document.querySelector('[name="sex"]').value = "";
+}
+
+let cadastrados = new Array();
+let I = 0;
