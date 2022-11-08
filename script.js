bfe44a84e00b9aa2198deccd11139c6ff3e51b86
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2022-11-08 14:54:39 -0300

    new checkboxes and messages

--- script.js
+++ script.js
@@ -1,22 +1,72 @@
+//redirect to other functions
 function functionRedirect() {
-  getValues();
+  invokeConstructor(getValues());
   clearInputs();
+  updateRegisteredList();
+  I++;
+  updateMessage();
 }
 
+//get user inputs
 function getValues() {
+  let userInformation = new Array(3);
   let fullName = document.querySelector('[name="fname"]').value;
-  console.log(fullName);
+  userInformation[0] = fullName;
   let birthDate = document.querySelector('[name="bdate"]').value;
-  console.log(birthDate);
-  let sexType = document.querySelector('[name="sex"]').value;
-  console.log(sexType);
+  userInformation[1] = birthDate;
+  let sexType = document.querySelector('[name="sex"]:checked').value;
+  userInformation[2] = sexType;
+  return userInformation;
 }
 
+//clear user inputs
 function clearInputs() {
   document.querySelector('[name="fname"]').value = "";
   document.querySelector('[name="bdate"]').value = "";
-  document.querySelector('[name="sex"]').value = "";
+  document.querySelectorAll('[name="sex"]')[0].checked = false;
+  document.querySelectorAll('[name="sex"]')[1].checked = false;
 }
 
 let cadastrados = new Array();
 let I = 0;
+function invokeConstructor(userInformation) {
+  cadastrados[I] = new Cadastro(
+    userInformation[0],
+    userInformation[1],
+    userInformation[2]
+  );
+}
+
+//constructor
+function Cadastro(name, birth, sex) {
+  this.name = name;
+  this.birth = adjustingBirth.call(this, birth);
+  this.sex = sex;
+}
+function adjustingBirth(birth) {
+  year = `${birth[0]}${birth[1]}${birth[2]}${birth[3]}`;
+  month = `${birth[5]}${birth[6]}`;
+  day = `${birth[8]}${birth[9]}`;
+  birthInternational = `${day}/${month}/${year}`;
+  return birthInternational;
+}
+//number of registereds and message relative to show/hide list
+function updateMessage() {
+  document.querySelector("#message").innerText =
+    "Total de usuários cadastrados: " + I;
+  document.querySelector("#message2").style.display = "block";
+}
+function updateRegisteredList() {
+  document.querySelector(
+    "#userlist"
+  ).innerHTML += ` Nome: <strong>${cadastrados[I].name}</strong> | Nascimento(DD/MM/AAAA): <strong>${cadastrados[I].birth}</strong> | Sexo: <strong>${cadastrados[I].sex}</strong><BR>`;
+}
+let slider = document.querySelector("#showdiv");
+
+slider.addEventListener("change", function () {
+  if (this.checked) {
+    document.querySelector("#userlist").style.display = "block";
+  } else {
+    document.querySelector("#userlist").style.display = "none";
+  }
+});
